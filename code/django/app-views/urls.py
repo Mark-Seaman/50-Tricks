e5c01abbbf5e01fcdbd0695ecec2f9c8abaf7d16
Mark Seaman <mark.seaman@shrinking-world.com>
2014-05-11 11:24:15 -0600

    Rename tricks

--- code/django/app-views/urls.py
+++ code/django/app-views/urls.py
@@ -2,13 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 
-
-# Hard-coded HTML
 def hello(request):
-    return HttpResponse('<h1>Template View Demo</h1><a href="about">About</a>')
+    return HttpResponse('<h1>Hello, Mark</h1><a href="about">About</a>')
 
-
-# Web page routing
 urlpatterns = patterns('',  
     url(r'^$', hello),
     url(r'^about/', TemplateView.as_view(template_name="about.html")),
