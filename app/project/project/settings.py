6df183276f3e4641cf276dc425b280e412fb81d5
Mark Seaman <mark.seaman@shrinking-world.com>
2014-05-14 10:58:38 -0600

    Setup static server

--- app/project/project/settings.py
+++ app/project/project/settings.py
@@ -4,13 +4,29 @@ from os.path import abspath,dirname,join
 
 DEBUG=True
 
+
 # App startup
 SECRET_KEY = 's!qs5!9(bhkv7#hn#172zm_*l#m)j(8lv1gj)#84p$9+^&amp;bn9e'
+
+
+# Location of router
 ROOT_URLCONF = 'project.urls'
+
+
+# Shim to run application from Apache
 WSGI_APPLICATION = 'project.wsgi.application'
-ROOT_DIR  = dirname(abspath(__file__))
+
+
+# Location of app directory
+ROOT_DIR  = dirname(dirname(abspath(__file__)))
 
 
 # Static server
-STATIC_ROOT = ''
 STATIC_URL = '/static/'
+STATIC_ROOT = ROOT_DIR+STATIC_URL
+
+
+# Loading templates
+TEMPLATE_DIRS = (
+    ROOT_DIR+"/templates",
+)
--- app/project/project/urls.py
+++ app/project/project/urls.py
@@ -1,9 +1,29 @@
+# urls.py
+# Simple page showing how to load twitter bootstrap
+
 from django.conf.urls import patterns, include, url
 from django.http import HttpResponse
+from django.template import loader,Context
 
-def hello(request):
-    return HttpResponse('<h1>Hello There</h1><a href="/">About</a>')
+from settings import STATIC_ROOT
 
+# Define the location of the static files
+static_dir   = { 'document_root' : STATIC_ROOT}
+
+
+# Create a page with a template
+def home(request): 
+    page = loader.get_template ('home.html')
+    return HttpResponse(page.render(Context({})))
+
+
+# Routes to the views
 urlpatterns = patterns('',  
-    url(r'^$', hello),
+
+    # static files
+    url(r'^static/(?P<path>.*)$',    'django.views.static.serve', static_dir),
+
+    # show the default page
+    url(r'^$', home),
+
 )
