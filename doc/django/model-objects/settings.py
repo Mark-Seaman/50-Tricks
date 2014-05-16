fb99525376109085075cdf0f4f6272be94bd1898
Mark Seaman <mark.seaman@shrinking-world.com>
2014-05-16 17:40:22 -0600

    Update model-objects example

--- doc/django/model-objects/settings.py
+++ doc/django/model-objects/settings.py
@@ -47,9 +47,9 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'project.urls'
+ROOT_URLCONF = 'urls'
 
-WSGI_APPLICATION = 'project.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 
 # Database
