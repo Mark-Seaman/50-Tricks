99c78f2998ef8d76f65247339d86cc8d307b8b49
Mark Seaman <mark.seaman@shrinking-world.com>
2014-05-16 16:44:13 -0600

    Update class template code

--- doc/django/view-class/app/urls.py
+++ doc/django/view-class/app/urls.py
@@ -5,8 +5,8 @@ from django.contrib import admin
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 
-from app.views import ContactList, ContactDetail, MyContactList
-from app.views import ContactCreate, ContactUpdate, ContactDelete
+from app.views   import ContactList, ContactDetail, ContactCreate, ContactUpdate, ContactDelete
+from app.myviews import MyContactDetail, MyContactList, MyContactCreate, MyContactUpdate 
 
 
 urlpatterns = patterns('',  
@@ -15,18 +15,24 @@ urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name="contact.html")),
    
     # Detail view
-    url(r'^contacts/(?P<pk>\d+)$', ContactDetail.as_view(), name='contact-detail'),
+    url(r'^contacts/(?P<pk>\d+)$',       ContactDetail.as_view(), name='contact-detail'),
 
     # List view
-    url(r'^contacts/$', ContactList.as_view(), name='contact_list'),
-    url(r'^my-contacts/$', MyContactList.as_view()),
+    url(r'^contacts/$',                  ContactList.as_view(), name='contact_list'),
 
     # Add view
-    url(r'contacts/add$', ContactCreate.as_view(), name='contact_add'),
+    url(r'contacts/add$',                ContactCreate.as_view(), name='contact_add'),
 
     # Update view
-    url(r'contacts/(?P<pk>\d+)/edit$', ContactUpdate.as_view(), name='contact_update'),
+    url(r'contacts/(?P<pk>\d+)/edit$',   ContactUpdate.as_view(), name='contact_update'),
 
     # Delete view
     url(r'contacts/(?P<pk>\d+)/delete$', ContactDelete.as_view(), name='contact_delete'),
+
+
+    # Specialized views
+    url(r'^my-contacts/(?P<pk>\d+)$',      MyContactDetail.as_view()),
+    url(r'^my-contacts/$',                 MyContactList.as_view()),
+    url(r'^my-contacts/add$',              MyContactCreate.as_view()),
+    url(r'^my-contacts/(?P<pk>\d+)/edit$', MyContactUpdate.as_view()),
 )
--- doc/django/view-class/app/views.py
+++ doc/django/view-class/app/views.py
@@ -1,28 +1,12 @@
 # views.py
-
-# Direct HTML from view
-
-# Template View -- class based view using a fixed template
-# View from model -- class based view 
-# Extra content in view
-# Viewing subsets of objects
-# Dynamic filtering
-# Doing extra work
-# Basic form editing
-# Model forms
-# Adding user info
+# Basic views
 
 from django.views.generic import ListView, DetailView
-from django.shortcuts import get_object_or_404
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
 
-
 from app.models import Contact
 
-
-#-----------------------------------------------------------------------------
-# List view
 
 # Basic list view with using a template
 class ContactList(ListView):
@@ -30,62 +14,27 @@ class ContactList(ListView):
     template_name = 'contact_list.html'
 
 
-
-# Filter the rows with a query
-class MyContactList(ListView):
-
-    template_name = 'contact_list.html'
-    #queryset = Contact.objects.filter(name__startswith='Contact ')
-
-    def get_queryset(self):
-        return Contact.objects.all()   #filter(user=self.request.user)
-
-
-#-----------------------------------------------------------------------------
-# Detail view
-
 # Basic detail view
 class ContactDetail(DetailView):
     model = Contact
     template_name = 'contact_detail.html'
 
-# Specialized detail view
-class MyContactDetail(DetailView):
-    #context_object_name = 'contact'
-    #queryset = Contact.objects.all()
-
-    model = Contact
-    template_name = 'contact_detail.html'
-
-    # Call the base implementation first to get a context
-    def get_context_data(self, **kwargs):
-        context = super(ContactDetail, self).get_context_data(**kwargs)
-        #context['name_list'] = [ 'Mark', 'Peter', 'Bob' ]
-        return context
-
-#-----------------------------------------------------------------------------
-# Edit view
 
 # Create view
 class ContactCreate(CreateView):
     model = Contact
-    fields = ['name','address', 'phone']
     template_name = 'contact_edit.html'
+
 
 # Update view
 class ContactUpdate(UpdateView):
     model = Contact
-    fields = ['name','address','phone']
     template_name = 'contact_edit.html'
 
-
-#-----------------------------------------------------------------------------
-# Edit view
 
 # Delete view
 class ContactDelete(DeleteView):
     model = Contact
     success_url = reverse_lazy('contact_list')
     template_name = 'contact_delete.html'
-
 
