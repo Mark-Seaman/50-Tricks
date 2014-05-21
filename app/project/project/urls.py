40581625497336f021f8553d145bffa26696ba41
Mark Seaman <mark.seaman@shrinking-world.com>
2014-05-21 12:49:46 -0600

    Revert code for initial app step

--- app/project/project/urls.py
+++ app/project/project/urls.py
@@ -1,29 +1,9 @@
-# urls.py
-# Simple page showing how to load twitter bootstrap
-
 from django.conf.urls import patterns, include, url
 from django.http import HttpResponse
-from django.template import loader,Context
 
-from settings import STATIC_ROOT
+def hello(request):
+    return HttpResponse('<h1>Hello There</h1><a href="/">About</a>')
 
-# Define the location of the static files
-static_dir   = { 'document_root' : STATIC_ROOT}
-
-
-# Create a page with a template
-def home(request): 
-    page = loader.get_template ('home.html')
-    return HttpResponse(page.render(Context({})))
-
-
-# Routes to the views
 urlpatterns = patterns('',  
-
-    # static files
-    url(r'^static/(?P<path>.*)$',    'django.views.static.serve', static_dir),
-
-    # show the default page
-    url(r'^$', home),
-
+    url(r'^$', hello),
 )
