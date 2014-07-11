5a8833c440ba96e4da73a9fe9d9cbac7626fabd5
Mark Seaman <mark.seaman@shrinking-world.com>
2014-07-11 16:56:24 -0600

    Reduce the view-function code

--- doc/django/view-function/app/views.py
+++ doc/django/view-function/app/views.py
@@ -11,66 +11,37 @@ from django.http        import HttpResponseRedirect, HttpResponse
 from app.models import Contact,ContactForm
 
 
-# Render a form for editing
-def form_render(request,template,data):
-    return render_to_response(template, data, context_instance=RequestContext(request))
-
-
-# Render a web page
-def render(request,template,data): 
-    page = loader.get_template (template)
-    return HttpResponse(page.render(Context(data)))
-
-
 # Render the appropriate doc view
 def contact_list (request):
-    content =  { 'object_list':Contact.objects.all() }
-    return render(request, 'contact.html', content)
+    page = loader.get_template ('contact.html')
+    context = Context({ 'object_list': Contact.objects.all() })
+    return HttpResponse(page.render(context))
 
 
-# Go to a specific page
-def redirect(request,page):
-    return ('/'+title) 
+# Get the object or create a new one
+def lookup(pk):
+    if pk != '0':
+        return Contact.objects.get(pk=pk)
+    else:
+        return Contact()
 
 
 # Render the add view
 def contact_edit(request,pk='0'):
-
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            if pk != '0':
-                c = Contact.objects.get(pk=pk)
-            else:
-                c = Contact()
+            c = lookup(pk)
             c.name = form.data['name']
             c.address = form.data['address']
             c.phone = form.data['phone']
             c.save()
             return HttpResponseRedirect('/')
     else: 
-        if pk != '0': 
-            c = Contact.objects.get(pk=pk)
-        else:
-            c = Contact()
-
+        c = lookup(pk)
     data =  { 'form': ContactForm (instance=c) }
-    return form_render (request, 'contact_edit.html', data)
-
-
-#-----------------------------------------------------------------------------
-
-# Basic list view with using a template
-class ContactList(ListView):
-    model = Contact
-    template_name = 'contact.html'
-
-    # # Filter the list of choices
-    # queryset = Contact.objects.filter(name__startswith='Contact ')
-
-    # # Use the request user to match the items
-    # def get_queryset(self):
-    #     return Contact.objects.filter(name=self.request.user.username)
+    template = 'contact_edit.html'
+    return render_to_response(template, data, context_instance=RequestContext(request))
 
 
 # Basic detail view
@@ -84,20 +55,6 @@ class ContactDetail(DetailView):
     #     context = super(MyContactDetail, self).get_context_data(**kwargs)
     #     context['name_list'] = [ 'Mark', 'Peter', 'Bob' ]
     #     return context
-
-
-# Create view
-class ContactCreate(CreateView):
-    model = Contact
-    fields = ['name','address', 'phone']
-    template_name = 'contact_edit.html'
-
-
-# Update view
-class ContactUpdate(UpdateView):
-    model = Contact
-    fields = ['name','address','phone']
-    template_name = 'contact_edit.html'
 
 
 # Delete view
